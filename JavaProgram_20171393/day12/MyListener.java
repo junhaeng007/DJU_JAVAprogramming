0ff9551c17df81073d25c825f353baa7583b519f
junhaeng007 <37131237+junhaeng007@users.noreply.github.com>
2018-11-27 17:08:05 +0900

    Update MyListener.java

--- JavaProgram_20171393/day12/MyListener.java
+++ JavaProgram_20171393/day12/MyListener.java
@@ -1,10 +1,13 @@
 package day12;
 
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
-
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class MyListener extends MouseMotionAdapter{
-	
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		super.mouseDragged(arg0);
+	}
 }
